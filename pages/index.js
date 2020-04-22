5dcf1ef27ab45ce99aec385679854663723edbd4
Jan <jan@walti.me>
2020-04-22 22:01:21 +0200

    updated title

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌊</text></svg>"
         />
-        <title>waves</title>
+        <title>walti.dev</title>
       </Head>
       <div
         style={{
